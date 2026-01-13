23d69e1ae0dec422ecbd355c2237bc29321e7957
Федір Яценко <32431794+Fedir-Yatsenko@users.noreply.github.com>
2026-01-13 17:07:15 +0200

    fix: dataset URNs (#104)

--- statgpt/common/data/sdmx/v21/datasource.py
+++ statgpt/common/data/sdmx/v21/datasource.py
@@ -32,7 +32,7 @@ from statgpt.common.data.sdmx.common import (
     SdmxDataSetConfigTemplate,
     SdmxDataSourceConfig,
     SdmxDimension,
-    UrnParser,
+    UrnReference,
 )
 from statgpt.common.data.sdmx.v21.dataflow_loader import DataflowLoader
 from statgpt.common.data.sdmx.v21.dataset import (
@@ -62,7 +62,6 @@ class Sdmx21DataSourceHandler(
 
     def __init__(self, config: SdmxDataSourceConfig):
         super().__init__(config)
-        self._urn_parser = UrnParser.create_default()
 
     @staticmethod
     def data_source_type() -> DataSourceType:
@@ -115,24 +114,24 @@ class Sdmx21DataSourceHandler(
         return res
 
     def _get_dataset_descriptor(self, dataflow: Dataflow) -> DataSetDescriptor:
-        urn = self._urn_parser.parse(dataflow.urn).get_short_urn()
+        urn = Urn.for_artifact(dataflow)
+        urn_ref = UrnReference.model_validate(urn, from_attributes=True)
         try:
-            config = self._create_config_for(dataflow, urn)
+            config = self._create_config_for(dataflow, urn_ref)
         except Exception:
             logger.warning(
                 f"Failed to create dataset config for dataflow urn={dataflow.urn!r}", exc_info=True
             )
-            config = SdmxDataSetConfigTemplate(urn=urn)
+            config = SdmxDataSetConfigTemplate(urn=urn_ref)
 
         return DataSetDescriptor(
-            source_id=urn,
             name=dataflow.name[self._config.locale],
             description=dataflow.description.localizations.get(self._config.locale),
             details=config,
         )
 
     @staticmethod
-    def _create_config_for(dataflow: Dataflow, short_urn: str) -> SdmxDataSetConfigTemplate:
+    def _create_config_for(dataflow: Dataflow, urn_ref: UrnReference) -> SdmxDataSetConfigTemplate:
         """We do our best to create a valid dataset configuration from the dataflow structure."""
 
         dimensions: dict[str, BaseDimensionConfig] = {}
@@ -152,7 +151,7 @@ class Sdmx21DataSourceHandler(
         if not dimensions:
             raise ValueError(f"Could not find any dimensions in dataflow {dataflow.urn!r}")
 
-        return SdmxDataSetConfigTemplate(urn=short_urn, dimensions=dimensions)
+        return SdmxDataSetConfigTemplate(urn=urn_ref, dimensions=dimensions)
 
     @property
     def entity_id(self) -> str:
