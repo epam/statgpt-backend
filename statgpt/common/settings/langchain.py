9e98dcdec635f9f47c4ae4dc89768d4fb6f106dd
Bahdan Kapionkin <bahdan_kapionkin@epam.com>
2026-02-03 15:20:08 +0200

    change default model back to 4.1

--- statgpt/common/settings/langchain.py
+++ statgpt/common/settings/langchain.py
@@ -24,7 +24,7 @@ class LangChainSettings(BaseSettings):
     )
 
     default_model: LLMModelsEnum = Field(
-        default=LLMModelsEnum.GPT_5_2_2025_12_11,
+        default=LLMModelsEnum.GPT_4_1_2025_04_14,
         description="Default LLM model for LangChain",
     )
 
