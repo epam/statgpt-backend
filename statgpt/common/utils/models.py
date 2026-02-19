7a565e477d0b6a7effaf2344e5aba4e5655d9214
Bahdan Kapionkin <bahdan_kapionkin@epam.com>
2026-02-19 11:21:30 +0200

    reformat

--- statgpt/common/utils/models.py
+++ statgpt/common/utils/models.py
@@ -29,17 +29,13 @@ def get_chat_model(
         timeout=timeout,  # timeouts are crucial!
     )
 
-    params.update(
-        model_config.model_dump(mode="json", exclude_none=True, exclude={"deployment"})
-    )
+    params.update(model_config.model_dump(mode="json", exclude_none=True, exclude={"deployment"}))
 
     if model_config.deployment.is_gpt_41_family:
         callback = BrokenResponseInterceptor(regex_pattern=r"\s{5,}")
         params.setdefault("callbacks", []).append(callback)
 
-    api_key_log = (
-        f"{api_key.get_secret_value()[:3]}*****{api_key.get_secret_value()[-2:]}"
-    )
+    api_key_log = f"{api_key.get_secret_value()[:3]}*****{api_key.get_secret_value()[-2:]}"
     logger.info(
         f"creating langchain LLM with the following params: {params}, Api key: {api_key_log}"
     )
@@ -60,9 +56,7 @@ def get_embeddings_model(
         max_retries=10,
         api_key=api_key,  # since we use SecretStr, it won't be logged
     )
-    api_key_log = (
-        f"{api_key.get_secret_value()[:3]}*****{api_key.get_secret_value()[-2:]}"
-    )
+    api_key_log = f"{api_key.get_secret_value()[:3]}*****{api_key.get_secret_value()[-2:]}"
     logger.info(
         f"creating langchain embeddings with the following params: {params}, Api key: {api_key_log}"
     )
