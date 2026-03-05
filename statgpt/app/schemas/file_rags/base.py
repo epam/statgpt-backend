bbf1054dc1b077c39bc9571921cccaa977c40c46
Bahdan Kapionkin <bahdan_kapionkin@epam.com>
2026-03-05 17:01:27 +0200

    upd defaults

--- statgpt/app/schemas/file_rags/base.py
+++ statgpt/app/schemas/file_rags/base.py
@@ -4,7 +4,7 @@ from statgpt.common.schemas import RAGVersion, ToolTypes
 
 class BaseRagState(ToolMessageState):
     type: ToolTypes = ToolTypes.FILE_RAG
-    version: RAGVersion = RAGVersion.DIAL
+    version: RAGVersion
 
-    response: str | None = None  # This is not needed since we have content field
-    answered_by: str | None = None
+    response: str = ""  # This is not needed since we have content field
+    answered_by: str = ""
