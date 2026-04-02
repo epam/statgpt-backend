78770e75cff1eeabc31e6bc04507e16c44fff068
Danylo_Kriachkov <danylo_kriachkov@epam.com>
2026-04-02 11:22:51 +0300

    fix: remove redundant import of TimeDimension in dataset.py

--- statgpt/common/data/sdmx/v21/dataset.py
+++ statgpt/common/data/sdmx/v21/dataset.py
@@ -18,7 +18,8 @@ import sdmx.model.common
 from dateutil.parser import parse
 from sdmx.message import DataMessage, StructureMessage
 from sdmx.model.common import Code
-from sdmx.model.v21 import DataflowDefinition as DataFlow, TimeDimension
+from sdmx.model.v21 import DataflowDefinition as DataFlow
+from sdmx.model.v21 import TimeDimension
 
 from statgpt.common.auth.auth_context import AuthContext
 from statgpt.common.data.base import (
