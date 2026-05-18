d6fc10ef0b059a94e3f00cbb5126393537ffe353
Azamat Kuzdibayev <azamat_kuzdibayev@.epam.com>
2026-05-18 13:27:27 +0500

    feat: add new gpt5.5 model's enum

--- statgpt/common/config/llm_models.py
+++ statgpt/common/config/llm_models.py
@@ -59,6 +59,7 @@ class LLMModelsEnum(StrEnum):
     GPT_5_2_2025_12_11 = "gpt-5.2-2025-12-11"
     GPT_5_4_2026_03_05 = "gpt-5.4-2026-03-05"
     GPT_5_4_MINI_2026_03_17 = "gpt-5.4-mini-2026-03-17"
+    GPT_5_5_2026_04_23 = "gpt-5.5-2026-04-23"
 
     @property
     def deployment_id(self) -> str:
@@ -83,4 +84,5 @@ class LLMModelsEnum(StrEnum):
             LLMModelsEnum.GPT_5_2_2025_12_11,
             LLMModelsEnum.GPT_5_4_2026_03_05,
             LLMModelsEnum.GPT_5_4_MINI_2026_03_17,
+            LLMModelsEnum.GPT_5_5_2026_04_23,
         }
